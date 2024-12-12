ef12f022b5a89ded66ad0ad2e1ce18cc7cd89325
Jinhua <jih216@usask.ca>
2024-12-11 22:01:11 -0600

    1

--- Myqueries.sql
+++ Myqueries.sql
@@ -6,4 +6,4 @@ INSERT INTO SubCategories (name, category_id) VALUES ('Test SubCategory', 1);
 
 INSERT INTO SubsubCategories (name, subcategory_id) VALUES ('Test SubSubCategory', 1);
 
-INSERT INTO users (username, password) VALUES ('JinhuaHe', '199802101');
+INSERT INTO users (username, password) VALUES ('JinhuaHe', '$2b$10$LjHlSu6/uHyaqGtL/E3L3e81Udin9c2W1OSaM67iIEdvylC4YrhCG');
