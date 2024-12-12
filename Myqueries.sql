0289f360869786bc07baed1be82d8ff937533351
Jinhua <jih216@usask.ca>
2024-12-11 20:02:06 -0600

    adding user table to coop with Login feature

--- Myqueries.sql
+++ Myqueries.sql
@@ -5,3 +5,5 @@ INSERT INTO Categories (name) VALUES ('Test Category');
 INSERT INTO SubCategories (name, category_id) VALUES ('Test SubCategory', 1);
 
 INSERT INTO SubsubCategories (name, subcategory_id) VALUES ('Test SubSubCategory', 1);
+
+INSERT INTO users (username, password) VALUES ('JinhuaHe', '199802101');
